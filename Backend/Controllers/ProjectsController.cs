1e8415f6bc55c9b41f57b5fed8438d97906d61f5
nagaokakid <nagaokakid@gmail.com>
2024-05-22 21:18:46 -0500

    Updated projects controller with try/catch blocks and status codes. May need to add other catch statements for incorrect requests (i.e. post w/o GUID).

--- Backend/Controllers/ProjectsController.cs
+++ Backend/Controllers/ProjectsController.cs
@@ -18,36 +18,91 @@ namespace Backend.Controllers
             _repository = new Repository<Project>(context);
         }
 
-        // GET: api/<ProjectsController>
+        // GET: api/projects
         [HttpGet]
-        public IEnumerable<string> Get()
+        public async Task<ActionResult<IQueryable<Project>>> Get()
         {
-            return new string[] { "value1", "value2" };
+            try
+            {
+                var projects = await _repository.GetAll();
+                return Ok(projects);
+            }
+            catch (Exception ex) 
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
-        // GET api/<ProjectsController>/5
+        // GET api/projects/GUID
         [HttpGet("{id}")]
-        public string Get(int id)
+        public async Task<ActionResult<Project>> Get(string id)
         {
-            return "value";
+            try
+            {
+                var project = await _repository.GetById(id);
+                if (project == null)
+                {
+                    return NotFound();
+                }
+                return Ok(project);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+
         }
 
-        // POST api/<ProjectsController>
+        // POST api/project
         [HttpPost]
-        public void Post([FromBody] string value)
+        public async Task<ActionResult<Project>> Post(Project newProject)
         {
+            try
+            {
+                var project = await _repository.Create(newProject);
+                return CreatedAtAction(nameof(Post), project);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
-        // PUT api/<ValuesController>/5
+        // PUT api/project/GUID
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public async Task<ActionResult<Project>> Put(string id, Project updatedProject)
         {
+            try
+            {
+                if (id != updatedProject.Id)
+                {
+                    return BadRequest("ID provided in URL doesn't match the ID in the request body.");
+                }
+                var result = await _repository.Update(updatedProject);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
 
-        // DELETE api/<ValuesController>/5
+        // DELETE api/project/GUID --> continue here
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public async Task<ActionResult<bool>> Delete(string id)
         {
+            try
+            {
+                bool result = await _repository.Delete(id);
+                if (!result)
+                {
+                    return NotFound("Resource with given ID could not be found.");
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
     }
 }
